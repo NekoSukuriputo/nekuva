a9e498f1de74daf278c6d19b4ddb51d5881e39a8
NekoSukuriputo <abdulrahmat97dev@gmail.com>
2026-05-30 11:51:07 +0700

    chore:remove build folder

--- composeApp/build/kspCaches/desktop/desktopMain/backups/kotlin/org/nekosukuriputo/nekuva/db/NekuvaDatabaseConstructor.kt
+++
@@ -1,8 +0,0 @@
-package org.nekosukuriputo.nekuva.db
-
-import androidx.room.RoomDatabaseConstructor
-
-public actual object NekuvaDatabaseConstructor : RoomDatabaseConstructor<NekuvaDatabase> {
-  actual override fun initialize(): NekuvaDatabase =
-      org.nekosukuriputo.nekuva.db.NekuvaDatabase_Impl()
-}
